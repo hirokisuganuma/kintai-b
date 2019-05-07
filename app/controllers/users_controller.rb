2495cfd8f17fee554875ca37f3caed6c46463879
hiroki suganuma <da78245@qa3.so-net.ne.jp>
2019-05-08 00:39:06 +0900

    0507

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,26 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        flash[:success] = "新規登録が完了しました"
+        redirect_to user_url(current_user)
+      else
+        render 'new'      
+      end
   end
 
   def edit
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
