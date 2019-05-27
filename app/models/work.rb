12868f720130d5022549941d64797b8c35836622
EC2 Default User <ec2-user@ip-172-31-26-169.ap-southeast-1.compute.internal>
2019-05-27 16:35:27 +0000

    0527

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,7 +2,7 @@ class Work < ApplicationRecord
   belongs_to :user
   validates :day,  presence: true
   validate :leaving_time_should_existence_attendance_time
-  # validate :leaving_time_should_early_attendance_time
+  validate :leaving_time_should_early_attendance_time
 
   def leaving_time_should_existence_attendance_time
     if attendance_time.nil?
@@ -10,9 +10,9 @@ class Work < ApplicationRecord
     end
   end
 
-  #def leaving_time_should_early_attendance_time
-  #  if attendance_time.to_s > leaving_time.to_s
-  #  errors.add(:leaving_time, ": 出社時間より退社時間が早い項目がありました")
-  #  end
-  #end
+  def leaving_time_should_early_attendance_time
+    if attendance_time.to_s < leaving_time.to_s
+    errors.add(:leaving_time, ": 出社時間より退社時間が早い項目がありました")
+    end
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class WorksControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get works_edit_url
-    assert_response :success
-  end
+ # test "should get edit" do
+  #  get works_edit_url
+  #  assert_response :success
+ # end
 
 end
